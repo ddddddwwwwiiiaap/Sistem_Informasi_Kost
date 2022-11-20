3240c49a40721ef938aafb0cc3d1d38488d2a050
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2022-11-20 13:45:23 +0700

    menambahkan kolom role&drop kolom role di add role

--- database/migrations/2022_11_20_062620_add_role_column_to_users_table.php
+++ database/migrations/2022_11_20_062620_add_role_column_to_users_table.php
@@ -14,7 +14,8 @@ class AddRoleColumnToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            //menambahkan kolom role
+            $table->enum('role', ['admin', 'customer'])->after('name');
         });
     }
 
@@ -26,7 +27,8 @@ class AddRoleColumnToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            //menambahkan drop kolom role
+            $table->dropColumn('role');
         });
     }
 }
