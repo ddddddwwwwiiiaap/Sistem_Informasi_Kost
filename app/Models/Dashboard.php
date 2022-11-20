9748e68e350a70fbfe0fd09e25f181cbee08fd0e
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2022-11-20 19:28:08 +0700

    Add Models

--- app/Models/Dashboard.php
+++ app/Models/Dashboard.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Auth;
 
 class Dashboard extends Model
 {
     use HasFactory;
+
+    //
+    public function customers()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public static function index()
+    {
+        return Dashboard::with('customer')->where('id', Auth::user()->id)->get();
+    }
 }
