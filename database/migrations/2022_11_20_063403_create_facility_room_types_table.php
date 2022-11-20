19671cf230664a50d4f60cad0a734cdb78faee06
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2022-11-20 13:36:42 +0700

    Create database migrations

---
+++ database/migrations/2022_11_20_063403_create_facility_room_types_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFacilityRoomTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facility_room_types', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facility_room_types');
+    }
+}
