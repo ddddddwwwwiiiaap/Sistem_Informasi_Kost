2c13006890cb188fa481c324c53bb846fdbe23d0
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2022-11-22 16:24:41 +0700

    Update source code UserSedder

--- database/seeders/UserSedder.php
+++ database/seeders/UserSedder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class UserSedder extends Seeder
 {
