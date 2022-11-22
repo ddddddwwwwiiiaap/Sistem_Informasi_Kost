f2b45f48e4a8abf0c76c5b28d2b63d7198745dc3
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2022-11-22 16:22:45 +0700

    Add source code file di folder seeders

--- database/seeders/UserSedder.php
+++ database/seeders/UserSedder.php
@@ -14,5 +14,21 @@ class UserSedder extends Seeder
     public function run()
     {
         //
+        User::insert([
+            [
+                'name' => 'Administrator',
+                'role' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('123'),
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Customer',
+                'role' => 'customer',
+                'email' => 'customer@gmail.com',
+                'password' => Hash::make('123'),
+                'created_at' => Carbon::now(),
+            ]
+        ]);
     }
 }
