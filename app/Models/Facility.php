343a1c6634d163990b5ced30248e097d5c23a035
ddddddwwwwiiiaap <dwiaprilyaanggoroputry02@gmail.com>
2022-11-22 17:48:37 +0700

    Update source code

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
-
 class Facility extends Model
 {
     use HasFactory;
