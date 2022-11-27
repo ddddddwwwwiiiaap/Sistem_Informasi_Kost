67d8d227755485e6b9929f9c9217e0c9dadda7a0
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2022-11-28 00:27:31 +0700

    menganti judul

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -22,7 +22,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
   <link rel="icon" type="image/png" href="{{ asset('http://files.web.neo.id/08/a3/08a3e4e2-1504-4355-ac9c-3b8e878c92f6.jpg') }}">
   <title>
-    Sistem Management Kost
+    Sistem Informasi Kost Griya Linggang Hayu
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
